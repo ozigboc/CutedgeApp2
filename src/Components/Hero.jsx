ca67997c9fae4a1b75d578c05d52aa66b83f52bb
ozigboc <ozigboc@gmail.com>
2024-04-24 03:22:09 +0100

    Components created

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className='hero'>
+            <h2>Discover The Technology that Works </h2>
+            <button>WORK WITH US</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
